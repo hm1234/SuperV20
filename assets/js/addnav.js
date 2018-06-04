2ecd73411bacf6571abf4e28d42fa1fa31b52b44
Dudeawsome <hassaanmirza123@gmail.com>
2018-06-04 17:45:29 -0400

    web

--- assets/js/addnav.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function()
-{
-  $.get("nav.html", function(html_string)
-   {
-      document.getElementById("navPanel").insertAdjacentHTML("afterbegin", html_string);
-      document.getElementById("nav").innerHTML = html_string;
-   },'html');
-});
